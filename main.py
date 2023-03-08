bd9ef0900c405a64a80076305429699172214a46
Douglas Storz <douglasstorz@gmail.com>
2023-03-08 15:05:52 -0500

    black format

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 def get_promoted_tweets() -> list:
     raise NotImplementedError
 
+
 def is_promoted(tweet) -> bool:
     raise NotImplementedError
+
 
 def click_ellipse_menu(tweet):
     raise NotImplementedError
 
+
 def click_not_interested():
-    """ The ellipse menu opens outside the tweet scope, so this grabs it and clicks from the global page view """
+    """The ellipse menu opens outside the tweet scope, so this grabs it and clicks from the global page view"""
     raise NotImplementedError
+
 
 def trigger_infinite_scroll():
     raise NotImplementedError
+
 
 def main():
     # Set up web driver
@@ -31,6 +36,7 @@ def main():
 
     # NOTE: May want to periodically refresh the page to avoid memory issues from loading too many tweets
     pass
+
 
 if __name__ == "__main__":
     main()
