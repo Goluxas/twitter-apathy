61c5e1e9ed9cbce45810284f9a818c029c7dcbf4
Douglas Storz <douglasstorz@gmail.com>
2023-03-08 20:48:37 -0500

    Infinite scrolling works

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pathlib import Path
 
 from selenium import webdriver
@@ -73,31 +74,40 @@ def main(profile_path: str, username: str, password: str):
             # Get the location of the ellipse menu
             menu = tweet.find_element(By.CSS_SELECTOR, 'div[data-testid="caret"]')
             menu_y = menu.location["y"]
+            current_scroll = driver.execute_script(
+                "return window.pageYOffset + window.innerHeight"
+            )
+            # TODO: replace 50 with menu element height or more
+            to_move = menu_y - current_scroll + 50
 
             # Scroll to tweet
             actions = ActionChains(driver)
-            actions.scroll_by_amount(delta_x=0, delta_y=menu_y)
-            actions.move_to_element(menu)
+            actions.scroll_by_amount(0, to_move)
             actions.perform()
 
+            # Open ellipse menu
             menu.click()
 
+            # Click Not Interested
+            # Next TODO: Popup menu may go above this, so select the Not interested in this ad <span> and move to it
+            actions.move_to_element(menu)
             actions.click()
             actions.perform()
-            # Click Not Interested
 
             # Log Success or Failure
+            # Success message:
             # Thanks. Twitter will use this to make your timeline better.
-            # Trigger infinite scroll
-            # Repeat until interrupted
 
         # Presumably at this point there are no more visible Promoted tweets
         # Scroll to the bottom and get a fresh set to examine
         logger.debug("All tweets processed, triggering infinite scroll.")
         driver.find_element(By.TAG_NAME, "html").send_keys(Keys.END)
-        WebDriverWait(driver, 10).until(lambda x: len(get_tweets(x)) > tweet_count)
+        time.sleep(5)
+        # the below needs some tuning so that the next set has finished loading before we proceed
+        # WebDriverWait(driver, 10).until(lambda x: len(get_tweets(x)) > tweet_count)
 
         # NOTE: May want to periodically refresh the page to avoid memory issues from loading too many tweets
+        # Seems like Twitter maintains a constant max of about 10-20 tweets, so we can ignore that!
 
 
 if __name__ == "__main__":
