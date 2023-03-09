54046f95f06976d9b417809e414f0852449f96d4
Douglas Storz <douglasstorz@gmail.com>
2023-03-08 21:27:21 -0500

    Added keyboard interrupt as an exit

--- main.py
+++ main.py
@@ -61,54 +61,60 @@ def main(profile_path: str, username: str, password: str):
     # A count of ads marked not interested
     boring_ads = 0
     while True:
-        # Get new Promoted Tweets
-        tweets = get_tweets(driver)
-        tweet_count = len(tweets)
-        promoted_tweets = list(filter(is_promoted, tweets))
+        # Kill this loop with CTRL+C when done
+        try:
+            # Get new Promoted Tweets
+            tweets = get_tweets(driver)
+            tweet_count = len(tweets)
+            promoted_tweets = list(filter(is_promoted, tweets))
 
-        logger.debug(f"Found {tweet_count} tweets and {len(promoted_tweets)} promoted tweets.")
+            logger.debug(f"Found {tweet_count} tweets and {len(promoted_tweets)} promoted tweets.")
 
-        for tweet in promoted_tweets:
-            print("in this loop")
-            # Get the location of the ellipse menu
-            menu = tweet.find_element(By.CSS_SELECTOR, 'div[data-testid="caret"]')
-            menu_y = menu.location["y"]
-            current_scroll = driver.execute_script("return window.pageYOffset + window.innerHeight")
-            # TODO: replace 50 with menu element height or more
-            to_move = menu_y - current_scroll + 100
+            for tweet in promoted_tweets:
+                print("in this loop")
+                # Get the location of the ellipse menu
+                menu = tweet.find_element(By.CSS_SELECTOR, 'div[data-testid="caret"]')
+                menu_y = menu.location["y"]
+                current_scroll = driver.execute_script("return window.pageYOffset + window.innerHeight")
+                # TODO: replace 50 with menu element height or more
+                to_move = menu_y - current_scroll + 100
 
-            # Scroll to tweet
-            actions = ActionChains(driver)
-            actions.scroll_by_amount(0, to_move)
-            actions.perform()
+                # Scroll to tweet
+                actions = ActionChains(driver)
+                actions.scroll_by_amount(0, to_move)
+                actions.perform()
 
-            # Open ellipse menu
-            menu.click()
+                # Open ellipse menu
+                menu.click()
 
-            # Click Not Interested
-            # Next TODO: Popup menu may go above this, so select the Not interested in this ad <span> and move to it
-            not_interested = get_not_interested_element(driver)
-            # actions.move_to_element(not_interested)
-            actions.click(not_interested)
-            actions.perform()
+                # Click Not Interested
+                # Next TODO: Popup menu may go above this, so select the Not interested in this ad <span> and move to it
+                not_interested = get_not_interested_element(driver)
+                # actions.move_to_element(not_interested)
+                actions.click(not_interested)
+                actions.perform()
 
-            # Log Success or Failure
-            if check_for_success(driver):
-                logger.info("Another uninteresting ad has been squelched")
-                boring_ads += 1
-            else:
-                logger.info("Something went wrong and the ad ignored being told it was uninteresting.")
+                # Log Success or Failure
+                if check_for_success(driver):
+                    logger.info("Another uninteresting ad has been squelched")
+                    boring_ads += 1
+                else:
+                    logger.info("Something went wrong and the ad ignored being told it was uninteresting.")
 
-        # Presumably at this point there are no more visible Promoted tweets
-        # Scroll to the bottom and get a fresh set to examine
-        logger.debug("All tweets processed, triggering infinite scroll.")
-        driver.find_element(By.TAG_NAME, "html").send_keys(Keys.END)
-        time.sleep(5)
-        # the below needs some tuning so that the next set has finished loading before we proceed
-        # WebDriverWait(driver, 10).until(lambda x: len(get_tweets(x)) > tweet_count)
+            # Presumably at this point there are no more visible Promoted tweets
+            # Scroll to the bottom and get a fresh set to examine
+            logger.debug("All tweets processed, triggering infinite scroll.")
+            driver.find_element(By.TAG_NAME, "html").send_keys(Keys.END)
+            time.sleep(5)
+            # the below needs some tuning so that the next set has finished loading before we proceed
+            # WebDriverWait(driver, 10).until(lambda x: len(get_tweets(x)) > tweet_count)
 
-        # NOTE: May want to periodically refresh the page to avoid memory issues from loading too many tweets
-        # Seems like Twitter maintains a constant max of about 10-20 tweets, so we can ignore that!
+            # NOTE: May want to periodically refresh the page to avoid memory issues from loading too many tweets
+            # Seems like Twitter maintains a constant max of about 10-20 tweets, so we can ignore that!
+        except KeyboardInterrupt:
+            break
+
+    logger.info(f"Squelched {boring_ads} ads during this run")
 
 
 if __name__ == "__main__":
